5b8d304e3018448042e75165be585653a6831ba7
Šteffl Daniel <danielsteffl19gmail.com>
2026-08-24 18:17:50 +0200

    v1.2.0 Ošetřen neplatný vstup, přidána nekonečná smyčka

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
-a = float(input("Zadej první číslo: "))
-b = float(input("Zadej druhé číslo: "))
+while True:
+    try:
+        a = float(input("Zadej první číslo: "))
+        b = float(input("Zadej druhé číslo: "))
+    except ValueError:
+        print("Neplatné číslo! Zkus to znovu.")
+        continue
 
-operace = input("Zadej operaci (+, -, *, /, :): ")
+    operace = input("Zadej operaci (+, -, *, /, :) nebo (q pro ukončení): ")
 
-match operace:
-    case "+":
-        print("Součet:", a + b)
-    case "-":
-        print("Rozdíl:", a - b)
-    case "*":
-        print("Součin:", a * b)
-    case "/" | ":":
-        if b == 0:
-            print("Nelze dělit nulou!")
-        else:
-            print("Podíl:", a / b)
-    case _:
-        print("Neplatná operace!")
+    match operace:
+        case "q":
+            print("Konec programu.")
+            break
+
+        case "+":
+            print("Součet:", a + b)
+
+        case "-":
+            print("Rozdíl:", a - b)
+
+        case "*":
+            print("Součin:", a * b)
+
+        case "/" | ":":
+            if b == 0:
+                print("Nelze dělit nulou!")
+            else:
+                print("Podíl:", a / b)
+
+        case _:
+            print("Neplatná operace!")
