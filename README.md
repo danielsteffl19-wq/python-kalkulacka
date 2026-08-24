b2746f97ef51137bfda56971592bcb09dde197ab
Šteffl Daniel <danielsteffl19gmail.com>
2026-08-24 17:08:24 +0200

    První verze Python kalkulačky

---
+++ README.md
@@ -0,0 +1,17 @@
+# Kalkulačka
+
+Moje první Python kalkulačka.
+
+## Funkce
+
+- sčítání
+- odčítání
+- násobení
+- dělení
+
+## Spuštění
+
+Program spusť pomocí:
+
+```bash
+python main.py
---
+++ main.py
@@ -0,0 +1,9 @@
+a = float(input("Zadej první číslo: "))
+b = float(input("Zadej druhé číslo: "))
+
+print("Součet:", a + b)
+print("Rozdíl:", a - b)
+print("Součin:", a * b)
+print("Podíl:", a / b)
+
+print("Ahoj, worlde!")
