a9ca1016fbe3a9d88bdf70d9e27873d0fa496abc
Šteffl Daniel <danielsteffl19gmail.com>
2026-08-24 18:03:22 +0200

    Přidána ochrana proti delění nulou

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 a = float(input("Zadej první číslo: "))
 b = float(input("Zadej druhé číslo: "))
 
-operace = input("Zadej operaci (+, -, *, /): ")
+operace = input("Zadej operaci (+, -, *, /, :): ")
 
 match operace:
     case "+":
@@ -10,7 +10,10 @@ match operace:
         print("Rozdíl:", a - b)
     case "*":
         print("Součin:", a * b)
-    case "/":
-        print("Podíl:", a / b)
+    case "/" | ":":
+        if b == 0:
+            print("Nelze dělit nulou!")
+        else:
+            print("Podíl:", a / b)
     case _:
         print("Neplatná operace!")
